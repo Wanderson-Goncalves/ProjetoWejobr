6d05ae872e0110dc4aa4c0846dc2a51d1a53787c
Wanderson-Goncalves <wgmbusinessintelligence@gmail.com>
2022-01-08 16:35:07 -0300

    Adicinando pagina para empresas

---
+++ Wejobr/Controllers/ParaEmpresasController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Wejobr.Controllers
+{
+    public class ParaEmpresasController : Controller
+    {
+      
+        private readonly ILogger<ParaEmpresasController> _logger;
+
+        public ParaEmpresasController(ILogger<ParaEmpresasController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult ParaEmpresas()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
+
